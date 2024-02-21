5884a9f5c13cfb15b281686c16e012dd64e65b34
Timofii0507 <timofiiyurkov@gamil.com>
2024-02-21 17:57:42 +0200

    Третє завдання  з  дз

--- lib/main3.dart
+++ lib/main3.dart
@@ -1,25 +1,23 @@
 import 'package:test/test.dart';
 
-int calculateSum(numbers) {
-  int sum = 0;
-  for (int number in numbers) {
+double calculateSum(List<double> numbers) {
+  double sum = 0;
+  for (double number in numbers) {
     sum += number;
   }
   return sum;
 }
 
 void main() {
-  group('calculateSum', () {
-    test('Повертає суму чисел у списку', () {
-      expect(calculateSum([1, 2, 3, 4, 5]), equals(15));
-    });
+  test('Сума чисел у пустому списку', () {
+    expect(calculateSum([]), equals(0));
+  });
 
-    test('Повертає 0 для пустого списку', () {
-      expect(calculateSum([]), equals(0));
-    });
+  test('Сума чисел у списку з одним елементом', () {
+    expect(calculateSum([5.0]), equals(5.0));
+  });
 
-    test('Повертає правильну суму для списку з одним елементом', () {
-      expect(calculateSum([10]), equals(10));
-    });
+  test('Сума чисел у списку з декількома елементами', () {
+    expect(calculateSum([1.0, 2.0, 3.0, 4.0, 5.0]), equals(15.0));
   });
 }
